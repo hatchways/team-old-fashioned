be869916a8da72d8e4c506980b8db85e4cbae8cf
Maluki Muthusi <malukimuthusi@gmail.com>
2021-07-13 05:42:08 +0300

    Contest Model
    Add Contest Model that has the fields required

---
+++ server/models/Contest.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const { User } = require('./User')
+
+const contestSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    prizeAmount: {
+        type: Number
+    },
+
+    deadline: {
+        type: Date,
+        required: true
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    created: {
+        type: Date,
+        Default: Date.now
+    }
+});
+
+const Contest = mongoose.model('Contest', contestSchema)
+model.exports = Contest
