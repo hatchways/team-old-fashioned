4f54134793a97fbbbf127d22797e1f62aa4b4402
Jerilene Medrano <jerixmx@gmail.com>
2021-07-15 04:21:19 +0800

    Replace fonts and primary color per mockup

--- client/src/themes/theme.ts
+++ client/src/themes/theme.ts
@@ -2,17 +2,27 @@ import { createMuiTheme } from '@material-ui/core';
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: '"Open Sans", "sans-serif", "Roboto"',
+    fontFamily: '"Poppins", "sans-serif", "Roboto"',
     fontSize: 12,
     button: {
       textTransform: 'none',
       fontWeight: 700,
     },
+    subtitle1: {
+      fontFamily: '"Poppins","sans-serif"',
+    },
   },
   palette: {
-    primary: { main: '#3A8DFF' },
+    primary: { main: '#000000' },
   },
   shape: {
     borderRadius: 5,
+  },
+  overrides: {
+    MuiTypography: {
+      subtitle1: {
+        fontWeight: 600,
+      },
+    },
   },
 });
