f9f3cda5c30b6736a5d8e11c9d771b56516e1ef6
Jerilene Medrano <jerixmx@gmail.com>
2021-07-15 04:26:59 +0800

    Show navbar instead of chat sidebar

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,13 @@
-import Grid from '@material-ui/core/Grid';
-import CssBaseline from '@material-ui/core/CssBaseline';
+// import Grid from '@material-ui/core/Grid';
+// import CssBaseline from '@material-ui/core/CssBaseline';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import useStyles from './useStyles';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
-import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
+import NavBar from '../../components/NavBar/NavBar';
 import { useEffect } from 'react';
 
 export default function Dashboard(): JSX.Element {
-  const classes = useStyles();
-
   const { loggedInUser } = useAuth();
   const { initSocket } = useSocket();
 
@@ -27,12 +24,5 @@ export default function Dashboard(): JSX.Element {
     return <CircularProgress />;
   }
 
-  return (
-    <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
-      <CssBaseline />
-      <Grid item className={classes.drawerWrapper}>
-        <ChatSideBanner loggedInUser={loggedInUser} />
-      </Grid>
-    </Grid>
-  );
+  return <NavBar />;
 }
