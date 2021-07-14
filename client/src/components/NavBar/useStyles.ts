67d4124eb8c156f484eff055cf88cd4be5110090
Jerilene Medrano <jerixmx@gmail.com>
2021-07-15 04:25:27 +0800

    Add Navbar with placeholder links

---
+++ client/src/components/NavBar/useStyles.ts
@@ -0,0 +1,52 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.palette.primary.main,
+    padding: '1rem 1rem',
+  },
+  title: {
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(4),
+  },
+  navItems: {
+    marginLeft: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  link: {
+    color: '#FFFFFF',
+    cursor: 'pointer',
+    textDecoration: 'none',
+    margin: theme.spacing(0, 4),
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+  createContestLink: {
+    color: '#FFFFFF',
+    cursor: 'pointer',
+    borderStyle: 'groove',
+    borderBlockColor: '#FFFFFF',
+    borderWidth: 1,
+    padding: theme.spacing(1.5, 4),
+    margin: theme.spacing(0, 6),
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+  profileImg: {
+    margin: theme.spacing(0, 0, 0, 4),
+  },
+  username: {
+    color: '#FFFFFF',
+    cursor: 'pointer',
+    textDecoration: 'none',
+    margin: theme.spacing(0, 0, 0, 2),
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+}));
+
+export default useStyles;
