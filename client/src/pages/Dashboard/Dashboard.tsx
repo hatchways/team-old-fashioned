f64f622d267a09e2e3464203aab2d7c180d4c4fa
Jerilene Medrano <jerixmx@gmail.com>
2021-07-15 04:26:59 +0800

    Show navbar instead of chat sidebar

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,17 +1,14 @@
-import Grid from '@material-ui/core/Grid';
-import CssBaseline from '@material-ui/core/CssBaseline';
+// import Grid from '@material-ui/core/Grid';
+// import CssBaseline from '@material-ui/core/CssBaseline';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import useStyles from './useStyles';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
-import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
+import NavBar from '../../components/NavBar/NavBar';
 import { useEffect } from 'react';
 import AuthHeader from '../../components/AuthHeader/AuthHeader';
 
 export default function Dashboard(): JSX.Element {
-  const classes = useStyles();
-
   const { loggedInUser } = useAuth();
   const { initSocket } = useSocket();
 
@@ -29,12 +26,8 @@ export default function Dashboard(): JSX.Element {
   }
 
   return (
-    <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
-      <CssBaseline />
-      <AuthHeader linkTo="/fileUpload" asideText="testing upload file page" btnText="open uploadFile page" />
-      <Grid item className={classes.drawerWrapper}>
-        <ChatSideBanner loggedInUser={loggedInUser} />
-      </Grid>
-    </Grid>
+    <div>
+      <AuthHeader linkTo="/fileUpload" asideText="testing upload file page" btnText="open uploadFile page" /> <NavBar />
+    </div>
   );
 }
