c0fe2786ff860fa005a2378c92f7ee00edd1623b
Jerilene Medrano <jerixmx@gmail.com>
2021-07-21 03:52:33 +0800

    Setup path for notification router

--- server/app.js
+++ server/app.js
@@ -1,62 +1,62 @@
-const colors = require("colors");
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
-const { notFound, errorHandler } = require("./middleware/error");
-const connectDB = require("./db");
-const { join } = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const colors = require('colors');
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const { notFound, errorHandler } = require('./middleware/error');
+const connectDB = require('./db');
+const { join } = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const authRouter = require("./routes/auth");
-const userRouter = require("./routes/user");
-const s3Router = require("./routes/s3");
-const contestRouter = require("./routes/contest")
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+const s3Router = require('./routes/s3');
+const contestRouter = require('./routes/contest');
+const notificationRouter = require('./routes/notification');
 
 const { json, urlencoded } = express;
-require("dotenv").config();
+require('dotenv').config();
 connectDB();
 const app = express();
 const server = http.createServer(app);
 
 const io = socketio(server, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("connected");
+io.on('connection', (socket) => {
+  console.log('connected');
 });
 
-if (process.env.NODE_ENV === "development") {
-  app.use(logger("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
 }
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
 
-app.use("/auth", authRouter);
-app.use("/users", userRouter);
-app.use("/upload", s3Router);
-app.use("/contest", contestRouter);
+app.use('/auth', authRouter);
+app.use('/users', userRouter);
+app.use('/upload', s3Router);
+app.use('/contest', contestRouter);
+app.use('/notifications', notificationRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
-  );
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html'));
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
+  app.get('/', (req, res) => {
+    res.send('API is running');
   });
 }
 
@@ -64,7 +64,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
+process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
