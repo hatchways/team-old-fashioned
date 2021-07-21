c74f5cde93600c455fcb69b679d429665409253a
Jerilene Medrano <jerixmx@gmail.com>
2021-07-21 18:52:44 +0800

    Correct controller function to avoid resending headers

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -6,25 +6,25 @@ const mongoose = require('mongoose');
 // handler for creating/sending a notification
 exports.createNotification = asyncHandler(async (req, res, next) => {
   const { type } = req.body;
-  if (type === 'submission') {
-    var { files, _id: submissionId, contestId, userId: senderId, submitDate: timeSent } = req.body;
-    var receiverId = await Contest.findById(contestId).then((contest) => {
-      return contest.user;
-    });
-  } else if (type === 'message') {
-  } else {
+  const typeList = ['submission', 'message'];
+  if (!typeList.includes(type)) {
     res.status(400);
     throw new Error('Unsupported notification type');
-  }
-
-  const notification = await Notification.create({ type, receiverId, senderId, contestId, submissionId, timeSent });
-
-  if (notification) {
-    res.status(201).json(notification);
   } else {
-    res.status(500);
-    throw new Error('invalid notification data');
-  }
+    if (type === 'submission') {
+      var { files, _id: submissionId, contestId, userId: senderId, submitDate: timeSent } = req.body;
+      var receiverId = await Contest.findById(contestId).then((contest) => {
+        return contest.user;
+      });
+    } else if (type === 'message') {
+    }
+    const notification = await Notification.create({ type, receiverId, senderId, contestId, submissionId, timeSent });
 
-  next();
+    if (notification) {
+      res.status(201).json(notification);
+    } else {
+      res.status(500);
+      throw new Error('invalid notification data');
+    }
+  }
 });
