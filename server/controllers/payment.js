503b0944427ede7d719cdb137611970afa0efb07
Jerilene Medrano <jerixmx@gmail.com>
2021-07-28 02:40:48 +0800

    Add controller and route for passing client secret
    Switch mongoose lookup to findById

--- server/controllers/payment.js
+++ server/controllers/payment.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const asyncHandler = require('express-async-handler');
 const User = require('../models/User');
+const Contest = require('../models/Contest');
 
 // Create parameters used for API testing purposes only
 exports.test = asyncHandler(async (req, res, next) => {
@@ -18,23 +19,35 @@ exports.test = asyncHandler(async (req, res, next) => {
 });
 
 exports.getPublicKey = (req, res) => {
-  res.json(process.env.STRIPE_PUBLIC_KEY);
+  res.json({ public_key: process.env.STRIPE_PUBLIC_KEY });
 };
 
-// Possibly triggered when client first creates a contest
-// TODO: Add intentID to user model
-exports.setupPaymentIntent = asyncHandler(async (req, res, next) => {
+exports.setupUserForPayments = asyncHandler(async (req, res, next) => {
   const customer = await stripe.customers.create();
-  const user = await User.findOneAndUpdate({ _id: req.user.id }, { stripe_id: customer.id });
+
   const intent = await stripe.setupIntents.create({
     customer: customer.id,
     payment_method_types: ['card'],
   });
   if (intent) {
+    const user = await User.findByIdAndUpdate(req.user.id, { stripe_id: customer.id, stripe_intent_id: intent.id });
     res.status(201).json({ customer_id: intent.customer, intent_id: intent.id, client_secret: intent.client_secret });
   } else {
     res.status(500);
     throw new Error('unable to set up payment intent');
+  }
+});
+
+exports.getSecret = asyncHandler(async (req, res, next) => {
+  const intent_id = await User.findById(req.user.id).then((user) => {
+    return user.stripe_intent_id;
+  });
+  const intent = await stripe.setupIntents.retrieve(intent_id);
+  if (intent) {
+    res.status(201).json({ client_secret: intent.client_secret });
+  } else {
+    res.status(500);
+    throw new Error('unable to retrieve secret');
   }
 });
 
@@ -55,15 +68,20 @@ exports.listPaymentMethods = asyncHandler(async (req, res, next) => {
 });
 
 exports.chargeCard = asyncHandler(async (req, res, next) => {
-  const customerId = await User.findOne(_id === req.user.id).then((user) => {
-    return user.stripe_customer_id;
+  const customerId = await User.findById(req.user.id).then((user) => {
+    return user.stripe_id;
   });
-  const { prizeAmount, cardId } = req.body;
+  console.log(customerId);
+  const { contestId, cardId } = req.body;
+  const prizeAmount = await Contest.findById(contestId).then((contest) => {
+    return contest.prizeAmount;
+  });
   const paymentIntent = await stripe.paymentIntents.create({
     customer: customerId,
     amount: prizeAmount,
     currency: 'usd',
     payment_method: cardId,
+    confirm: true,
   });
   if (paymentIntent) {
     res.status(201).json(paymentIntent);
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middleware/auth');
-const { test, getPublicKey, setupPaymentIntent, listPaymentMethods } = require('../controllers/payment');
+const {
+  test,
+  getPublicKey,
+  getSecret,
+  setupUserForPayments,
+  listPaymentMethods,
+  getPaymentIntent,
+  chargeCard,
+} = require('../controllers/payment');
 
+router.route('/').get(protect, listPaymentMethods);
 router.route('/test').post(protect, test);
-
-router.route('/setup').post(protect, setupPaymentIntent);
 router.route('/public-key').get(protect, getPublicKey);
-router.route('/payment-methods').get(protect, listPaymentMethods);
+router.route('/secret').get(protect, getSecret);
+router.route('/setup').post(protect, setupUserForPayments);
+router.route('/pay').post(protect, chargeCard);
 
 module.exports = router;
