833ec82e277e7799e2d85e82f322f939970148db
Jerilene Medrano <jerixmx@gmail.com>
2021-07-28 21:10:59 +0800

    Use Notification Interface for mapping

--- client/src/pages/Notifications/NotificationsList/NotificationsList.tsx
+++ client/src/pages/Notifications/NotificationsList/NotificationsList.tsx
@@ -24,58 +24,40 @@ export function NotificationsList({ notifications, type }: Props): JSX.Element {
   // };
   return (
     <List component="nav" className={classes.root} aria-label="notifications">
-      {notifications.map(
-        (notification: {
-          _id: string;
-          type: string;
-          senderId: {
-            _id: string;
-            username: string;
-          };
-          receiverId: string;
-          contestId: {
-            _id: string;
-            title: string;
-          };
-          submissionId?: string;
-          createdAt: Date;
-          readStatus: boolean;
-          photo: string;
-        }) => (
-          <>
-            <ListItem
-              button
-              key={(notification.type, notification.photo, notification.senderId.username, notification.type)}
-            >
-              <Grid container={true} alignItems="center" justify="center" spacing={2} wrap="nowrap">
-                <Grid item xs={2} container direction="column" justify="flex-end" alignItems="flex-end">
-                  <Avatar alt="Notification Thumbnail" src={notification.photo} />
-                </Grid>
-                <Grid item xs={9}>
-                  <Typography variant="body2" noWrap={type === 'dropdown' ? true : false}>
-                    {notification.type === 'submission' ? (
-                      <span>
-                        <b>{notification.senderId.username}</b> submitted a design to your contest{' '}
-                        <b>{notification.contestId.title}</b>.
-                      </span>
-                    ) : (
-                      <span>
-                        <b>{notification.senderId.username}</b> sent you a message.
-                      </span>
-                    )}
-                    <br />
-                    <i>{timeSince(notification.createdAt)} ago</i>
-                  </Typography>
-                </Grid>
-                <Grid item xs={1} justify="flex-start" alignItems="flex-start">
-                  {notification.readStatus ? '  ' : <FiberManualRecordIcon className={classes.readIndicator} />}
-                </Grid>
+      {notifications.map((notification: Notification) => (
+        <>
+          <ListItem
+            button
+            key={(notification.type, notification.photo, notification.senderId.username, notification.type)}
+          >
+            <Grid container={true} alignItems="center" justify="center" spacing={2} wrap="nowrap">
+              <Grid item xs={2} container direction="column" justify="flex-end" alignItems="flex-end">
+                <Avatar alt="Notification Thumbnail" src={notification.photo} />
               </Grid>
-            </ListItem>
-            <Divider />
-          </>
-        ),
-      )}
+              <Grid item xs={9}>
+                <Typography variant="body2" noWrap={type === 'dropdown' ? true : false}>
+                  {notification.type === 'submission' ? (
+                    <span>
+                      <b>{notification.senderId.username}</b> submitted a design to your contest{' '}
+                      <b>{notification.contestId.title}</b>.
+                    </span>
+                  ) : (
+                    <span>
+                      <b>{notification.senderId.username}</b> sent you a message.
+                    </span>
+                  )}
+                  <br />
+                  <i>{timeSince(notification.createdAt)} ago</i>
+                </Typography>
+              </Grid>
+              <Grid item xs={1} justify="flex-start" alignItems="flex-start">
+                {notification.readStatus ? '  ' : <FiberManualRecordIcon className={classes.readIndicator} />}
+              </Grid>
+            </Grid>
+          </ListItem>
+          <Divider />
+        </>
+      ))}
     </List>
   );
 }
