7873204fd996dcdb2bfe1754fa7860bf207aa10b
Jerilene Medrano <jerixmx@gmail.com>
2021-08-03 04:05:43 +0800

    Convert API Call to generic profile photo uploading

--- client/src/helpers/APICalls/updateProfilePhoto.ts
+++ client/src/helpers/APICalls/updateProfilePhoto.ts
@@ -1,18 +1,18 @@
 import { AuthApiDataSuccess } from '../../interface/AuthApiData';
 import { FetchOptions } from '../../interface/FetchOptions';
 
-const updateProfilePicture = async (url: string): Promise<AuthApiDataSuccess> => {
+const updateProfilePhoto = async (imageType: string, url: string): Promise<AuthApiDataSuccess> => {
   const fetchOptions: FetchOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ url }),
+    body: JSON.stringify({ imageType, url }),
     credentials: 'include',
   };
-  return await fetch(`/users/profile-pic/`, fetchOptions)
+  return await fetch(`/users/profile-photo/`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
     }));
 };
 
-export default updateProfilePicture;
+export default updateProfilePhoto;
