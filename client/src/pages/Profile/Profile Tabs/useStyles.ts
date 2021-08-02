8be52a69dc99cc0d344993c9c8a55e91e055ed90
Jerilene Medrano <jerixmx@gmail.com>
2021-08-02 21:03:04 +0800

    Add profile tabs component

---
+++ client/src/pages/Profile/Profile Tabs/useStyles.ts
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: 'transparent',
+  },
+  panel: {
+    backgroundColor: '#ffffff',
+  },
+  caption: {
+    fontWeight: 600,
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+  },
+}));
+
+export default useStyles;
