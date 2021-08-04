a5ab4f62acd4d6fd2f019fd6e91a98220035aded
yliu298 <lookinglyb@hotmail.com>
2021-08-04 12:44:32 -0400

    forgot-password-#86

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,6 +20,8 @@ import DesignSubmit from './components/DesignSubmit/DesignSubmit';
 import ProfileSetting from './pages/ProfileSetting/ProfileSetting';
 import Message from './pages/Message/Message';
 import Discovery from './pages/Discovery/Discovery';
+import ForgetPassword from './pages/ResetPassword/ForgetPassword/ForgetPassword';
+import ResetPassword from './pages/ResetPassword/ResetPassword/ResetPassword';
 
 function App(): JSX.Element {
   return (
@@ -34,6 +36,8 @@ function App(): JSX.Element {
                   <Route path="/" component={NavBar} />
                   <Route exact path="/login" component={Login} />
                   <Route exact path="/signup" component={Signup} />
+                  <Route path="/email/reset-password/:token" component={ResetPassword} />
+                  <Route exact path="/forget-password" component={ForgetPassword} />
                   <ProtectedRoute exact path="/file-upload/:id" component={DesignSubmit} />
                   <ProtectedRoute exact path="/setting" component={ProfileSetting} />
                   {/* Replace component with Discovery Page */}
