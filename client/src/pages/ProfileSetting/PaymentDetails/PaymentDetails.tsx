ca0519dc919ca033bcd4173237a38015e2194bcd
Eric Silva <62892917+eric-silva-61@users.noreply.github.com>
2021-08-04 14:09:39 -0600

    Added list of payment details if they exist

--- client/src/pages/ProfileSetting/PaymentDetails/PaymentDetails.tsx
+++ client/src/pages/ProfileSetting/PaymentDetails/PaymentDetails.tsx
@@ -2,26 +2,28 @@ import { FC, useEffect, useState } from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import PaymentForm from '../../../components/PaymentForm/PaymentForm';
+import PaymentMethods from '../../../components/PaymentMethods/PaymentMethods';
 import PageContainer from '../../../components/PageContainer/PageContainer';
+import { IPaymentMethod } from '../../../interface/Payments';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY as string);
 
 const PaymentDetails: FC = (): JSX.Element => {
-  const [paymentMethods, setPaymentMethods] = useState();
+  const [paymentMethods, setPaymentMethods] = useState<IPaymentMethod[]>();
   useEffect(() => {
     const getPaymentMethods = async () => {
       const response = await fetch('/payments/');
       if (response.ok) {
         const data = await response.json();
-        return data;
+        setPaymentMethods(data as IPaymentMethod[]);
       }
     };
-    //setPaymentMethods(getPaymentMethods());
+    getPaymentMethods();
   }, [setPaymentMethods]);
   return (
     <Elements stripe={stripePromise}>
       <PageContainer titleEl="Payment Details">
-        <PaymentForm />
+        {paymentMethods ? <PaymentMethods paymentMethods={paymentMethods} /> : <PaymentForm />}
       </PageContainer>
     </Elements>
   );
